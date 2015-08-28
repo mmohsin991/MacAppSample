27b1bd214e160b8cac1c75727976663b0c150f35
Mohsin <muhammad.mohsin.991@gmail.com>
2015-08-28 15:09:06 +0500

    Initial Commit

---
+++ MacAppSample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacAppSample
+//
+//  Created by Mohsin on 28/08/2015.
+//  Copyright (c) 2015 Mohsin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
